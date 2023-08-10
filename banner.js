8d2caed74d3a0a237e9a6ed5197c008412132c0b
vinsebastian <dloglog@gmail.com>
2023-08-11 06:21:09 +0700

    Create banner.js

---
+++ banner.js
@@ -0,0 +1,12 @@
+const atOptions = {
+    'key': '7274f145ccd626ed577bfbc551f6d59e',
+    'format': 'iframe',
+    'height': 250,
+    'width': 300,
+    'params': {}
+};
+
+const script = document.createElement('script');
+script.type = "text/javascript";
+script.src = `http${location.protocol === 'https:' ? 's' : ''}://sluicebigheartedpeevish.com/7274f145ccd626ed577bfbc551f6d59e/invoke.js`;
+document.body.appendChild(script);
